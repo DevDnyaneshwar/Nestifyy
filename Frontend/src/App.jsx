bb601f924b1a3889d962e15f5cfcce700ed7b3b9
DevDnyaneshwar <210303108321@paruluniversity.ac.in>
2025-07-16 14:03:27 +0530

    solve bug in app.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,7 @@
 // src/App.jsx
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { AppContext } from './context/Appcontext';
+
 
 // Import Components
 import Navbar from './components/Navbar.jsx';
